37298bd43d48f717ed2bdb9fac493d35a39b9395
tlizri <tlizri@outlook.com>
2022-11-08 11:35:26 +0100

    Completed exercise five

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+import ExerciseFive.isBisiesto as e5
+
+
 def main():
-    pass
+    year = input('Introduzca un año: ')
+    if e5.isBisiesto(int(year)):
+        print('El año ' + year + ' es bisiesto')
+    else:
+        print('El año ' + year + ' no es bisiesto')
 
 
 if __name__ == '__main__':
